15b5d29fed3ad9b6f67ae9cc87b2ec7c1afbf568
Jasdeepsingh20 <jasdeepsingh2022@gmail.com>
2022-02-08 22:24:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
+const categoryRoute = require('./routes/categories');
+const multer = require('multer');
+const path = require('path');
+
+const cors = require('cors');
+
+// CORS MIDDLEWARE
+app.use(
+    cors({
+        origin: 'https://reverent-beaver-4a1418.netlify.app',
+    })
+);
+
+dotenv.config();
+
+app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, '/images')));
+
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: true,
+    })
+    .then(console.log('Connected to MongoDB'))
+    .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+});
+
+const upload = multer({ storage: storage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+    res.status(200).json('File has been uploaded');
+});
+
+app.get('/', (req, res) => {
+    res.send('App deployed');
+});
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
+app.use('/api/categories', categoryRoute);
+
+
+app.use(express.static(path.join(__dirname, "/client/build")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
+    console.log('Backend is running.');
+});
